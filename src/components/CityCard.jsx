1728d30687c568e28623e99d67649126e483f5c7
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-06 13:10:37 +0100

    changement proptypes

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
-import propTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 const CityCard = ({ city }) => {
   return (
     <Link to={`${city}`} className='city-card'>
       <h3 className='city-card__name'>{city}</h3>
-      <img  alt="" style={{
-        height: '100%',
-         width: '100%',
-          borderRadius: '.5rem',
-          backgroundImage: `url(../images/${city}.jpg)`,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          }} />
+      <img src={`/images/${city}.jpg`} alt="" style={{height: '100%', width: '100%', borderRadius: '.5rem'}} />
     </Link>
   )
-}
-
-CityCard.propTypes = {
-    city: propTypes.string.isRequired,
 }
 
 export default CityCard
