c4a8e3d5cc49b19db32730f6f5d567b80006a129
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-06 10:04:35 +0100

    modif css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import CityCards from '../components/CityCards'
 const Home = () => {
   return (
     <div className='home'>
-      <div className='home__content-wrapper'>
         <h1 className='home__content-title'>Choisissez une ville pour afficher les horraires des trains</h1>
+      <div className='home__content-wrapper'>
         <CityCards />
       </div>
     </div>
