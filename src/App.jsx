f3a078b33aea6dc5b01f8bd1b5fd5bc1294d63db
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-06 16:53:44 +0100

    add snowfall

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import { Route, Routes } from 'react-router-dom'
 import TrainStation from './components/TrainStation'
 import City from './pages/City'
 import Home from './pages/Home'
+import Snowfall from 'react-snowfall'
 
 function App() {
     return (
         <div className='App'>
+                <Snowfall
+                style={{
+                    position: 'fixed',
+                    width: '100vw',
+                    height: '100vh',
+                }}
+            />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/:city' element={<City />}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import CityCards from '../components/CityCards'
 
 const Home = () => {
-  return (
-    <div className='home'>
-        <h1 className='home__content-title'>Choisissez une ville pour afficher les horaires des trains</h1>
-      <div className='home__content-wrapper'>
-        <CityCards />
-      </div>
-    </div>
-  )
+    return (
+        <div className='home'>
+            <h1 className='home__content-title'>Choisissez une ville pour afficher les horaires des trains</h1>
+            <div className='home__content-wrapper'>
+                <CityCards />
+            </div>
+        </div>
+    )
 }
 
 export default Home
