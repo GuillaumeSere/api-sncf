663566c95b9c8e0b20ef22d5f4d5e4941aa591fd
Guillaume Sere <guillaumesere@hotmail.fr>
2023-11-28 14:41:49 +0100

    add input search

--- src/components/CityCards.jsx
+++ src/components/CityCards.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import CityCard from './CityCard'
 import stations from '../gares.json'
 
-const CityCards = () => {
+const CityCards = ({ searchTerm }) => {
 
     const cities = Object.keys(stations)
 
+     // Filtrer les villes en fonction de la recherche
+     const filteredCities = cities.filter((city) =>
+     city.toLowerCase().includes(searchTerm.toLowerCase())
+ );
+
+
   return (
     <div className='city-cards'>
-        {cities.map((city) => (
+        {filteredCities.map((city) => (
          <CityCard key={city} city={city} />
          ))
         }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CityCards from '../components/CityCards'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 
 const Home = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
     return (
         <>
-        <Header />
-        <div className='home'>
-            <div className='home__content-wrapper'>
-            <h1 className='home__content-title'>Choisissez une<span> ville </span>pour afficher les <span>horaires</span> des trains</h1>
-                <CityCards />
+            <Header />
+            <div className='home'>
+                <div className='home__content-wrapper'>
+                    <h1 className='home__content-title'>Choisissez une<span> ville </span>pour afficher les <span>horaires</span> des trains</h1>
+                    <input
+                        className='input-search'
+                        type='text'
+                        placeholder='Rechercher une gare...'
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
+                    <CityCards searchTerm={searchTerm} />
+                </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </>
     )
 }
