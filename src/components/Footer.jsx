6f2e09fed6540e2dbd47294eafd197b1279995a5
Guillaume Sere <guillaumesere@hotmail.fr>
2024-01-09 22:56:22 +0100

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import git from "../components/icons8-github-64.png";
+import linkedin from "../components/icons8-logo-linkedin-64.png";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-       <p> Copyrights 2022, Developed by <span>Guillaume SERE </span>with React</p>
+    <div class='footer'>
+    <div class="footer-links">
+        <a href="https://www.linkedin.com/in/guillaume-séré" target="_blank" rel="noreferrer"><img
+                src={linkedin} class='link2' /></a>
+        <p> <span>&copy;</span> Guillaume SERE</p>
+        <a href="https://github.com/GuillaumeSere" target="_blank" rel="noreferrer"><img
+                src={git} class='link1' /></a>
+
     </div>
+</div>
   )
 }
 
