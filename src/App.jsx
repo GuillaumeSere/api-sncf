4bb494e61f81a62b4ac9a9dc7565f1499e2d43a5
Guillaume Sere <guillaumesere@hotmail.fr>
2024-05-19 15:53:31 +0200

    modif

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import TrainStation from './components/TrainStation'
 import City from './pages/City'
 import Home from './pages/Home'
@@ -7,23 +7,21 @@ import Snowfall from 'react-snowfall'
 
 function App() {
     return (
-        <Router basename={process.env.PUBLIC_URL}>
-            <div className='App'>
-                { /*  <Snowfall
+        <div className='App'>
+              { /*  <Snowfall
                 style={{
                     position: 'fixed',
                     width: '100vw',
                     height: '100vh',
                 }}
             />*/ }
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/:city' element={<City />}>
-                        <Route path=':codeStation' element={<TrainStation />} />
-                    </Route>
-                </Routes>
-            </div>
-        </Router>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/:city' element={<City />}>
+                    <Route path=':codeStation' element={<TrainStation />} />
+                </Route>
+            </Routes>
+        </div>
     )
 }
 
