6309dffbb7d63c953992a817548998f64f7c24e6
Guillaume Sere <guillaumesere@hotmail.fr>
2025-04-17 12:52:16 +0200

    modif fonctionality

--- src/components/TrainStations.jsx
+++ src/components/TrainStations.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 function TrainStations ({ stations }){
+    const navigate = useNavigate()
+    
+    // Si une seule gare, rediriger automatiquement
+    React.useEffect(() => {
+        const stationKeys = Object.keys(stations)
+        if (stationKeys.length === 1) {
+            const [stationName] = stationKeys
+            navigate(stations[stationName])
+        }
+    }, [stations, navigate])
+
     return (
         <div className='train-stations'>
             {Object.keys(stations).map((stationName) => (
